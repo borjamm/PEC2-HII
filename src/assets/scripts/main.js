99967681ac413cc107327ee3cbec9ed7f7d1ba0d
borjamm <codebmmjob@gmail.com>
2021-04-26 22:42:00 +0100

    Header, footer and blog

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -15,3 +15,24 @@ import * as bootstrap from "bootstrap";
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 })();
+
+(function () {
+  "use strict";
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
